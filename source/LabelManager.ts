295600e8dc308b29f70407a1664b7f913c0d93c6
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-06-30 19:12:26 +0200

    fix: Lint

--- source/LabelManager.ts
+++ source/LabelManager.ts
@@ -1,4 +1,4 @@
-import core from "@actions/core";
+import type core from "@actions/core";
 import { Context } from "@actions/github/lib/context.js";
 import { type GitHub } from "@actions/github/lib/utils.js";
 import YAML from "yaml";
